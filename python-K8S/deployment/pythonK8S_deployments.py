dbfbb6b944b1b4870847584b5b513025d664af7d
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2022-10-13 13:15:55 -0300

    update create file python

--- python-K8S/deployment/pythonK8S_deployments.py
+++ python-K8S/deployment/pythonK8S_deployments.py
@@ -4,7 +4,7 @@ import yaml
 from kubernetes import client, config
 config.load_kube_config()
 
-with open(path.join(path.dirname(__file__), "nginx-deployment.yaml")) as f:
+with open(path.join(path.dirname(__file__), "somefile.yaml")) as f:
     dep = yaml.safe_load(f)
     k8s_apps_v1 = client.AppsV1Api()
     resp = k8s_apps_v1.create_namespaced_deployment(
